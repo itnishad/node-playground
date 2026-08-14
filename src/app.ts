1438bed940e35274495e1af39205d4750a0f9f97
Nishad <mdfaysalhassan@gmail.com>
2026-08-14 11:56:39 +0600

    feat: add express setup with basic todo endpoints and TypeScript support
    - Added express as a dependency and @types/express for TypeScript definitions.
    - Created app.ts for Express server setup with basic GET and POST /todos routes.
    - Configured express middleware for JSON and URL-encoded data handling.
    - Updated tsconfig.json with additional settings for Express compatibility.

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+// Express Setup
+import express, {type Express, type Request, type Response} from 'express'
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const staticPath = join(__dirname, '..', '/public')
+const app: Express = express();
+
+// Express er property dekbo
+
+console.log(app.locals)
+
+console.log("x-powered-by: ",app.get('x-powered-by'))
+app.set('x-powered-by', false)
+console.log("x-powered-by2: ",app.get('x-powered-by'))
+
+// Express er method dekbo
+
+// express configuration
+app.use(express.json({limit: '200kb'}))
+app.use(express.urlencoded({extended: true, limit:'5mb'}))
+app.use('/static', express.static(staticPath))
+
+app.get('/todos', (req, res) =>{
+    res.send("Hello World")
+})
+
+app.post('/todos', (req, res) =>{
+    console.log(req.body)
+
+    res.send("Hello World Post")
+})
+
+app.listen(4000, ()=>{
+    console.log("App is running on port 4000")
+})
+
+//TODO:amader basic todo project te express e convert korbo
